39314d85dbbd44f9442053550a0b395d31c71dfa
Isaac Tee <iztyj.work@gmail.com>
2023-12-15 12:16:43 +0800

    added validation for task 6

--- src/main/java/vttp/ssf/assessment/eventmanagement/controllers/RegistrationController.java
+++ src/main/java/vttp/ssf/assessment/eventmanagement/controllers/RegistrationController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import vttp.ssf.assessment.eventmanagement.models.Event;
 import vttp.ssf.assessment.eventmanagement.models.Participant;
 import vttp.ssf.assessment.eventmanagement.services.DatabaseService;
@@ -25,11 +28,12 @@ public class RegistrationController {
 
     // TODO: Task 6
     @GetMapping(path = "/events/register/{id}")
-	public String register(@PathVariable("id") String id, Model model){
+	public String register(@PathVariable("id") String id, Model model, HttpSession session){
         List<Event> eventList = dataSvc.getEvents();
         Integer idNumber = 4 - Integer.parseInt(id);
         Event event = eventList.get(idNumber);
         model.addAttribute("event", event);
+        session.setAttribute("event", event);
         model.addAttribute("participant", new Participant());
         return "eventregister";
 	}
@@ -37,7 +41,15 @@ public class RegistrationController {
 
     // TODO: Task 7
     @PostMapping(path = "/registration/register")
-    public String processRegistration(){
+    public String processRegistration(@Valid @ModelAttribute("participant") Participant participant,
+     BindingResult result, Model model, HttpSession session){
+        System.out.println("Participant: "+participant);
+        if(result.hasErrors()){
+            System.out.println("has errors");
+            model.addAttribute(session.getAttribute("event"));
+            //model.addAttribute("participant", new Participant());
+            return "eventregister";
+        }
         return "ok";
     }
 }
--- src/main/java/vttp/ssf/assessment/eventmanagement/models/Participant.java
+++ src/main/java/vttp/ssf/assessment/eventmanagement/models/Participant.java
@@ -2,21 +2,45 @@ package vttp.ssf.assessment.eventmanagement.models;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class Participant {
+
+    @NotEmpty(message = "name required")
+    @Size(min = 5, max = 25, message = "Must be between 5 and 25 characters")
     private String fullName;
 
+
+    @NotNull(message = "DOB required")
+    @DateTimeFormat(pattern= "yyyy-MM-dd")
+    @Past(message = "birthday must be in the past")
     private Date dob;
 
+    @NotEmpty(message = "Email required")
+    @Email(message = "Must be valid email format")
+    @Size(max = 50, message = "Max 50 characters for email")
     private String email;
 
+    @Pattern(regexp = "(8|9)[0-9]{7}", message = "Must start with 8 or 9 followed by 7 digits")
     private String phoneNo;
 
     private String gender;
 
+    @Min(value = 1, message = "Minimum 1 ticket")
+    @Max(value = 3, message = "Maximum 3 tickets")
     private Integer tickets;
 
     public Participant(){
-        
+
     }
 
     public Participant(String fullName, Date dob, String email, String phoneNo, String gender, Integer tickets) {
